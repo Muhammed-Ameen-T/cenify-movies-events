77ac80f7f44e2d6252fbd8e896238f6e1296a2e2
Your Name <mhdameent2006@gmail.com>
2025-07-08 13:27:54 +0530

    fix: edit in get seat selection controller

--- backend/src/presentation/routes/seatSelection.routes.ts
+++ backend/src/presentation/routes/seatSelection.routes.ts
@@ -3,19 +3,17 @@ import { Router } from 'express';
 import { container } from 'tsyringe';
 import { ISeatSelectionController } from '../controllers/interface/seatSelection.controller.interface';
 import { verifyAccessToken } from '../middleware/verifyToken.middleware';
+import { authorizeRoles } from '../middleware/rbac.middleware';
 
 const router = Router();
 const seatSelectionController =
   container.resolve<ISeatSelectionController>('SeatSelectionController');
 
-router.get('/:showId', verifyAccessToken, (req, res) =>
+router.get('/:showId', (req, res) =>
   seatSelectionController.getSeatSelection(req, res),
 );
-router.post('/:showId/select', verifyAccessToken, (req, res) =>
+router.post('/:showId/select', verifyAccessToken,authorizeRoles(['user']), (req, res) =>
   seatSelectionController.selectSeats(req, res),
 );
 
 export default router;
-// router.get('/:showId', authMiddleware, socketMiddleware, (req, res) => seatSelectionController.getSeatSelection(req, res));
-// router.post('/:showId/select', authMiddleware, socketMiddleware, (req, res) => seatSelectionController.selectSeats(req, res));
-// import { socketMiddleware } from '../middleware/socket.middleware';
